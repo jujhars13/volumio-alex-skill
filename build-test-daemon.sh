831f39346be3aa7347b12d896763483dc2625fff
Jujhar Singh <jujhar@jujhar.com>
2020-04-18 22:21:02 +0100

    parking

--- build-test-daemon.sh
+++ build-test-daemon.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 (cd sqs-poll-daemon && go fmt main.go && go install)
+(cd sqs-poll-daemon && go fmt main.go && GOOS=linux GOARCH=arm GOARM=5 go build -o volumio-sqs-poll-daemon-raspi)
 
 export SQS_ENDPOINT="https://sqs.eu-west-1.amazonaws.com/753637769290/jujhar-test-volumio"
 export DOMAIN="volumio"
